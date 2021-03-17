445393c4d402087ce2652323492ba34adf18e9b6
Samy0412 <samantha.gadet@gmail.com>
2021-03-17 14:29:45 -0400

    try number 10

--- src/db/index.js
+++ src/db/index.js
@@ -3,13 +3,15 @@ let dbParams = {
 };
 if (process.env.DATABASE_URL) {
   dbParams.connectionString = process.env.DATABASE_URL;
+  dbParams.sslmode = require;
 } else {
   dbParams = {
-    host: "ls-6a1e60392078f1a3943200850aef0602d1eb53a9.csdsebrve8sw.ca-central-1.rds.amazonaws.com",
-    port: "5432",
-    user: "dbmasteruser",
-    password: "<p6o<kHt(d=c^1)ItYvp,ad=blKbp`wa",
-    database: "test",
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    sslmode:require
   };
 }
 
