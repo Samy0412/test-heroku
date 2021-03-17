9e6ce3085976fb58ab1ce6766da61b2106b771a7
Samy0412 <samantha.gadet@gmail.com>
2021-03-17 12:55:50 -0400

    reorganize

--- src/application.js
+++ src/application.js
@@ -1,60 +1,51 @@
-// environment variables setup
-const path = require("path");
-
-const ENV = process.env.NODE_ENV || "development";
-const PATH = path.resolve(__dirname, "../.env." + ENV);
-
-require("dotenv").config({ path: PATH });
-
-
-
 //Dependencies
 const express = require("express");
+const app = express();
 const bodyparser = require("body-parser");
 const helmet = require("helmet");
 const cors = require("cors");
+const path = require("path");
 
 
-//Creating an Express server
-const app = express();
-const PORT = process.env.PORT || 8001;
+//Route path variables
+const indexRoutes = require("./routes/index");
+const neighbourhoodRoutes = require("./routes/neighbourhood");
+
 
 //PG database client/connection setup
-// const { Pool } = require('pg')
-// const dbParams = require('./db/index.js');
-// console.log("dbparams:",dbParams)
-// const db = new Pool(dbParams);
+const { Pool } = require('pg')
+const dbParams = require('./db/index.js');
+console.log("dbparams:",dbParams)
+const db = new Pool(dbParams);
 
-// db
-//   .connect()
-//   .catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
+db
+  .connect()
+  .catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
 
+
+module.exports = function application (ENV){
 
   app.use(cors());
   app.use(helmet());
   app.use(bodyparser.json());
   app.use(bodyparser.urlencoded({ extended: true }));
  
-  
-
-//Route path variables
-const indexRoutes = require("./routes/index");
-const neighbourhoodRoutes = require("./routes/neighbourhood");
-
   //Routes
   app.use("/", indexRoutes);
-  // app.use("/neighbourhood", neighbourhoodRoutes(db));
+  app.use("/neighbourhood", neighbourhoodRoutes(db));
+
+
+  app.close = function () {
+    return db.end();
+  };
+
+  return app;
+}
+
   
 
 
-  app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}.`);
-  });
-  
-
-  // app.close = function () {
-  //   return db.end();
-  // };
+ 
 
 
  
