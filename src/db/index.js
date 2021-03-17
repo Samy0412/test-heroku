5eadb6fefbc65b04876c43036826bbc6d843ad84
Samy0412 <samantha.gadet@gmail.com>
2021-03-17 14:03:42 -0400

    correct connection string

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,6 @@
 
 let dbParams = {
-  connectionString = "postgres://grxbyxtblwrryf:a828f1d1c4b42ff747eb43d1290d31daa63e099261e24625a368d237188d27eb@ec2-34-195-233-155.compute-1.amazonaws.com:5432/d3eoakaofaq40a"
+  connectionString : "postgres://grxbyxtblwrryf:a828f1d1c4b42ff747eb43d1290d31daa63e099261e24625a368d237188d27eb@ec2-34-195-233-155.compute-1.amazonaws.com:5432/d3eoakaofaq40a"
 };
 // if (process.env.DATABASE_URL) {
 //   dbParams.connectionString = process.env.DATABASE_URL;
