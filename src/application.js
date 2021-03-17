2767e6e8ed4c3186a09aa25a063531b8e616d2f5
Samy0412 <samantha.gadet@gmail.com>
2021-03-17 12:02:45 -0400

    first commit

--- src/application.js
+++ src/application.js
@@ -1,29 +1,55 @@
+// environment variables setup
+const path = require("path");
 
+const ENV = process.env.NODE_ENV || "development";
+const PATH = path.resolve(__dirname, "../.env." + ENV);
+
+require("dotenv").config({ path: PATH });
+
+
+
+//Dependencies
 const express = require("express");
 const bodyparser = require("body-parser");
 const helmet = require("helmet");
 const cors = require("cors");
-const app = express();
-const db = require("./db");
 
+
+//Creating an Express server
+const app = express();
+const PORT = process.env.PORT || 8001;
+
+//PG database client/connection setup
+const { Pool } = require('pg')
+const dbParams = require('./db/index.js');
+console.log("dbparams:",dbParams)
+const db = new Pool(dbParams);
+
+db
+  .connect()
+  .catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
+
+
+  app.use(cors());
+  app.use(helmet());
+  app.use(bodyparser.json());
+  app.use(bodyparser.urlencoded({ extended: true }));
+ 
+  
 
 //Route path variables
 const indexRoutes = require("./routes/index");
 const neighbourhoodRoutes = require("./routes/neighbourhood");
 
-
-module.exports = function application(
-  ENV
-) {
-  app.use(cors());
-  app.use(helmet());
-  app.use(bodyparser.json());
-  app.use(bodyparser.urlencoded({ extended: true }));
-  
-
   //Routes
   app.use("/", indexRoutes);
   app.use("/neighbourhood", neighbourhoodRoutes(db));
+  
+
+
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}.`);
+  });
   
 
   app.close = function () {
@@ -31,5 +57,5 @@ module.exports = function application(
   };
 
 
-  return app;
-};
+ 
+
--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,17 @@
-const pg = require("pg");
 
-const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || "",
-});
+let dbParams = {};
+if (process.env.DATABASE_URL) {
+  dbParams.connectionString = process.env.DATABASE_URL;
+  dbParams.sslmode = require;
+} else {
+  dbParams = {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    sslmode:require
+  };
+}
 
-
-client
-  .connect()
-  .catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
-
-module.exports = client;
+module.exports = dbParams;
